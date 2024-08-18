64d157d7b8b0cf314d5b913e6dda570ce9ab9c21
hchair <hchair@FUTURE.myguest.virtualbox.org>
2024-08-18 06:52:48 +0200

    for real tomorrow Ill complete philos creation then strart the routine

--- philo.c
+++ philo.c
@@ -23,8 +23,19 @@ int main(int ac, char **av)
         }
     // start to fill our info and creat the philosophers
     table.end_simulation = false;
-    table.philos = malloc(sizeof(table.philos) * table.total);
+    table.philos = malloc(sizeof(t_philo) * table.total);
     if (table.philos == NULL)
         return (0);
-    
+        // initialise philos
+    for (int i = 0; i < table.total; i++) 
+    {
+        table.philos[i].id = i;
+        table.philos[i].left_fork = &table.forks[i];
+        table.philos[i].right_fork = &table.forks[(i + 1) % table.total];
+        // /gettimeofday(&table.philos[i].last_meal, NULL);
+        table.philos[i].meal_cnt = 0;
+        table.philos[i].full = false;
+        table.philos[i].thread_id = 0; // Initialize to 0, not the actual thread ID
+        table.philos[i].menu = &table;
+    }
 }
